4f0e450fe3c3ea5b1ad41429a628b7495d1ef694
W0ut <exor.ultor@gmail.com>
2021-02-09 23:02:35 +0600

    Add files via upload

--- scripts/webstart.sh
+++ scripts/webstart.sh
@@ -44,101 +44,96 @@ echo -en " ${WH}[${RD}!${WH}] ${GR}Enter your ip address? [${WH}192.168.1.100${G
 read DOMAINIP
 echo -en " ${WH}[${RD}!${WH}] ${GR}Enter your host name? [${WH}mysite${GR}] ${CY}> ${RD}"
 read DOMAINNAME
-if [[ ${ANSWER} == "HTTP" || ${ANSWER} == "http" || -z ${ANSWER} ]]
+if [[ -n ${DOMAINIP} && -n ${DOMAINNAME} ]]
     then
         clear
         echo -e ""
-        echo -e " ${NM}${PR}=============== HTTP ================="
-        THESERV2=`cat /etc/apache2/sites-available/000-default.conf | grep -Ea "ServerName.*" | grep -Po '\S.*?(?=\s*$)' | grep -v '^#'`
-        THEREDIRECT=`cat /etc/apache2/sites-available/000-default.conf | grep -Ea "Redirect.*" | grep -Po '\S.*?(?=\s*$)' | grep -v '^#'`
+        service apache2 start &>/dev/null
+        # service apache2 reload &>/dev/null
+        if [[ ${ANSWER} == "HTTP" || ${ANSWER} == "http" || -z ${ANSWER} ]]
+            then
+                echo -e " ${NM}${PR}=============== HTTP ================="
+                echo -e ""
+            else
+                TYPEHOST="https"
+                echo -e " ${NM}${PR}=============== HTTPS ================"
+                echo -e ""
+                a2enmod ssl &>/dev/null
+                a2ensite default-ssl &>/dev/null
+                service apache2 reload &>/dev/null
+                mkdir /etc/apache2/ssl &>/dev/null
+                xterm -T "Create SSL Certificate" -geometry 180x40 -e "openssl req -x509 -nodes -days 700 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt"
+                THESERV=`cat /etc/apache2/sites-enabled/default-ssl.conf | grep -nPo '\S.*?(?=\s*$)' | grep -Ea "ServerName.*" | awk -F ':' '{print $2, $1}' | grep -v '^#' | awk '{print $NF}'`
+                THESSLCRT=`cat /etc/apache2/sites-enabled/default-ssl.conf | grep -nPo '\S.*?(?=\s*$)' | grep -Ea "SSLCertificateFile.*" | awk -F ':' '{print $2, $1}' | grep -v '^#' | awk '{print $NF}'`
+                THESSLKEY=`cat /etc/apache2/sites-enabled/default-ssl.conf | grep -nPo '\S.*?(?=\s*$)' | grep -Ea "SSLCertificateKeyFile.*" | awk -F ':' '{print $2, $1}' | grep -v '^#' | awk '{print $NF}'`
+                # ══ Change /etc/apache2/sites-enabled/default-ssl.conf ════════╗ START ╔═
+                if [[ -n "${THESERV}" ]]
+                    then
+                        sed -i "${THESERV}c\ServerName ${DOMAINNAME}\n/" /etc/apache2/sites-enabled/default-ssl.conf
+                    else
+                        sed -i "s/<VirtualHost.*/<VirtualHost *:443>\n              ServerName ${DOMAINNAME}\n/" /etc/apache2/sites-enabled/default-ssl.conf
+                fi
+                if [[ -n "${THESSLCRT}" ]]
+                    then
+                        sed -i "${THESSLCRT}c\SSLCertificateFile \/etc\/apache2\/ssl\/apache.crt/" /etc/apache2/sites-enabled/default-ssl.conf
+                fi
+                if [[ -n "${THESSLKEY}" ]]
+                    then
+                        sed -i "${THESSLKEY}c\SSLCertificateKeyFile \/etc\/apache2\/ssl\/apache.key/" /etc/apache2/sites-enabled/default-ssl.conf
+                fi
+                # ══ Change /etc/apache2/sites-enabled/default-ssl.conf ════════╝  END  ╚═
+        fi
+        THESERV2=`cat /etc/apache2/sites-available/000-default.conf | grep -nPo '\S.*?(?=\s*$)' | grep -Ea "ServerName.*" | awk -F ':' '{print $2, $1}' | grep -v '^#' | awk '{print $NF}'`
+        THEREDIRECT=`cat /etc/apache2/sites-available/000-default.conf | grep -nPo '\S.*?(?=\s*$)' | grep -Ea "Redirect.*" | awk -F ':' '{print $2, $1}' | grep -v '^#' | awk '{print $NF}'`
         # ══ Change /etc/apache2/sites-available/000-default.conf ════════╗ START ╔═
         if [[ -n "${THESERV2}" ]]
             then
-                sed -i "s/${THESERV2}/ServerName ${DOMAINNAME}\n/" /etc/apache2/sites-available/000-default.conf
+                sed -i "${THESERV2}c\/ServerName ${DOMAINNAME}\n/" /etc/apache2/sites-available/000-default.conf
             else
-                sed -i "s/<VirtualHost.*/<VirtualHost *:80>\n       ServerName ${DOMAINNAME}\n/" /etc/apache2/sites-available/000-default.conf
+                sed -i "s/<VirtualHost.*/<VirtualHost *:80>\n              ServerName ${DOMAINNAME}\n/" /etc/apache2/sites-available/000-default.conf
         fi
         if [[ -n "${THEREDIRECT}" ]]
             then
-                sed -i "s/${THEREDIRECT}/ /" /etc/apache2/sites-available/000-default.conf
-        fi
-        # ══ Change /etc/apache2/sites-available/000-default.conf ════════╝  END  ╚═
-    else
-        TYPEHOST="https"
-        clear
-        echo -e ""
-        echo -e " ${NM}${PR}=============== HTTPS ================"
-        echo -e ""
-        a2enmod ssl &>/dev/null
-        a2ensite default-ssl &>/dev/null
-        service apache2 reload &>/dev/null
-        mkdir /etc/apache2/ssl &>/dev/null
-        THESERV=`cat /etc/apache2/sites-enabled/default-ssl.conf | grep -Ea "ServerName.*" | grep -Po '\S.*?(?=\s*$)' | grep -v '^#'`
-        THESSLCRT=`cat /etc/apache2/sites-enabled/default-ssl.conf | grep -Ea "SSLCertificateFile.*\/etc.*" | grep -Po '\S.*?(?=\s*$)' | grep -v '^#'`
-        THESSLKEY=`cat /etc/apache2/sites-enabled/default-ssl.conf | grep -Ea "SSLCertificateKeyFile.*\/etc.*" | grep -Po '\S.*?(?=\s*$)' | grep -v '^#'`
-        THESERV2=`cat /etc/apache2/sites-available/000-default.conf | grep -Ea "ServerName.*" | grep -Po '\S.*?(?=\s*$)' | grep -v '^#'`
-        THEREDIRECT=`cat /etc/apache2/sites-available/000-default.conf | grep -Ea "Redirect.*" | grep -Po '\S.*?(?=\s*$)' | grep -v '^#'`
-        # ══ Change /etc/apache2/sites-enabled/default-ssl.conf ════════╗ START ╔═
-        if [[ -n "${THESERV}" ]]
-            then
-                sed -i "s/${THESERV}/ServerName ${DOMAINNAME}\n/" /etc/apache2/sites-enabled/default-ssl.conf
-            else
-                sed -i "s/<VirtualHost.*/<VirtualHost *:443>\n       ServerName ${DOMAINNAME}\n/" /etc/apache2/sites-enabled/default-ssl.conf
-        fi
-        if [[ -n "${THESSLCRT}" ]]
-            then
-                sed -i "s/${THESSLCRT}/SSLCertificateFile \/etc\/apache2\/ssl\/apache.crt/" /etc/apache2/sites-enabled/default-ssl.conf
-        fi
-        if [[ -n "${THESSLKEY}" ]]
-            then
-                sed -i "s/${THESSLKEY}/SSLCertificateKeyFile \/etc\/apache2\/ssl\/apache.crt/" /etc/apache2/sites-enabled/default-ssl.conf
-        fi
-        if [[ -n "${THESSLKEY}" ]]
-            then
-                sed -i "s/${THESSLKEY}/SSLCertificateKeyFile \/etc\/apache2\/ssl\/apache.crt/" /etc/apache2/sites-enabled/default-ssl.conf
-        fi
-        # ══ Change /etc/apache2/sites-enabled/default-ssl.conf ════════╝  END  ╚═
-        # ══ Change /etc/apache2/sites-available/000-default.conf ════════╗ START ╔═
-        if [[ -n "${THESERV2}" ]]
-            then
-                sed -i "s/${THESERV2}/ServerName ${DOMAINNAME}\n/" /etc/apache2/sites-available/000-default.conf
-            else
-                sed -i "s/<VirtualHost.*/<VirtualHost *:80>\n       ServerName ${DOMAINNAME}\n/" /etc/apache2/sites-available/000-default.conf
-        fi
-        if [[ -n "${THEREDIRECT}" ]]
-            then
-                sed -i "s/${THEREDIRECT}/Redirect \"/\" \"https://${DOMAINNAME}/\"/" /etc/apache2/sites-available/000-default.conf
+                sed -i "${THEREDIRECT}c\/Redirect \"/\" \"${TYPEHOST}://${DOMAINNAME}/\"/" /etc/apache2/sites-available/000-default.conf
             else
                 sed -i "s/ServerName.*/ServerName ${DOMAINNAME}\n       Redirect "/" "https://${DOMAINNAME}/"/" /etc/apache2/sites-available/000-default.conf
         fi
         # ══ Change /etc/apache2/sites-available/000-default.conf ════════╝  END  ╚═
-fi
-service apache2 start &>/dev/null
-service apache2 reload &>/dev/null
-# ══ Change host file ════════╗ START ╔═
-THEFILEHOST=`ls /etc | grep -aoE "^host$"`
-THEFILEHOSTS=`ls /etc | grep -aoE "^host$"`
-THEHOST=`cat /etc/host | grep -o -a -E "${DOMAINIP}.*"`
-THEHOSTS=`cat /etc/hosts | grep -o -a -E "${DOMAINIP}.*"`
-if [[ -n "${THEHOST}" && -n "${THEFILEHOST}" ]]
-    then
-        sed -i "s/${THEHOST}.*/${DOMAINIP} ${DOMAINNAME}\n/" /etc/host
+        # service apache2 start &>/dev/null
+        service apache2 reload &>/dev/null
+        # ══ Change host file ════════╗ START ╔═
+        THEFILEHOST=`ls /etc | grep -aoE "^host$"`
+        THEFILEHOSTS=`ls /etc | grep -aoE "^host$"`
+        if [[ -n "${THEFILEHOST}" ]]
+            then
+                THEHOST=`cat /etc/host | grep -o -a -E "${DOMAINIP}.*"`
+                if [[ -n "${THEHOST}" ]]
+                    then
+                        sed -i "s/${THEHOST}.*/${DOMAINIP} ${DOMAINNAME}\n/" /etc/host
+                    else
+                        echo -e "${DOMAINIP} ${DOMAINNAME}" >> /etc/host
+                fi
+        fi
+        if [[ -n "${THEFILEHOSTS}" ]]
+            then
+                THEHOSTS=`cat /etc/hosts | grep -o -a -E "${DOMAINIP}.*"`
+                if [[ -n "${THEHOSTS}" ]]
+                    then
+                        sed -i "s/${THEHOSTS}.*/${DOMAINIP} ${DOMAINNAME}\n/" /etc/hosts
+                    else
+                        echo -e "${DOMAINIP} ${DOMAINNAME}" >> /etc/hosts
+                fi
+        fi
+        # ══ Change host file ════════╝  END  ╚═
+        service apache2 restart &>/dev/null
+        clear
+        echo -e " ${NM}${WH}The server ${TYPEHOST}://${DOMAINNAME} is ${RD} running!"
+        echo -e ""
+        echo -e " ${NM}${BL}TYPE       ${NM}${CY}>>  ${NM}${WH}${TYPEHOST}"
+        echo -e " ${NM}${BL}IP ADDRESS ${NM}${CY}>>  ${NM}${WH}${DOMAINIP}"
+        echo -e " ${NM}${BL}DOMAIN NAME${NM}${CY}>>  ${NM}${GR}${DOMAINNAME}"
+        echo -e ""
     else
-        echo -e "${DOMAINIP} ${DOMAINNAME}" >> /etc/host
+        echo -e " ${WH}[${RD}!${WH}] ${GR}You can't work with empty values!"
 fi
-if [[ -n "${THEHOSTS}" && -n "${THEFILEHOSTS}" ]]
-    then
-        sed -i "s/${THEHOSTS}.*/${DOMAINIP} ${DOMAINNAME}\n/" /etc/hosts
-    else
-        echo -e "${DOMAINIP} ${DOMAINNAME}" >> /etc/hosts
-fi
-# ══ Change host file ════════╝  END  ╚═
-service apache2 restart &>/dev/null
-clear
-echo -e " ${NM}${WH}The server ${TYPEHOST}://${DOMAINNAME} is ${RD} running!"
-echo -e ""
-echo -e " ${NM}${BL}TYPE       ${NM}${CY}>>  ${NM}${WH}${TYPEHOST}"
-echo -e " ${NM}${BL}IP ADDRESS ${NM}${CY}>>  ${NM}${WH}${DOMAINIP}"
-echo -e " ${NM}${BL}DOMAIN NAME${NM}${CY}>>  ${NM}${GR}${DOMAINNAME}"
-echo -e ""
 exit
